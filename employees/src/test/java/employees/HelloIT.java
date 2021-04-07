e3d4acfaa96e0310ececd672230114535ee113c4
iviczian <viczian.istvan@gmail.com>
2021-04-07 13:19:25 +0200

    Integrációs tesztelés.

---
+++ employees/src/test/java/employees/HelloIT.java
@@ -0,0 +1,20 @@
+package employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HelloIT {
+
+    @Autowired
+    HelloController helloController;
+
+    @Test
+    void testSayHello() {
+        String result = helloController.sayHello();
+        assertThat(result).startsWith("*** Hello Spring Boot Service");
+    }
+}
